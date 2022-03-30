741f5f7ddb3c2b398257b00d19a0ac7b273b2ef8
Botond Ferenczi <botondferenczi98@gmail.com>
2022-03-30 18:56:14 +0300

    euler_from_quaternion

--- controller.py
+++ controller.py
@@ -1,11 +1,23 @@
+#!/usr/bin/env python3
+
 import rospy
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
 def callback(msg):
+#   Print time
     now = rospy.get_rostime()
     print(rospy.loginfo("Secs: %i Nsecs: %i", now.secs, now.nsecs))
-    print(msg.pose.pose)
+#   print(msg.pose.pose)
+
+#   Get euler from quaternion
+    orientation_q = msg.pose.pose.orientation
+    orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+    (roll, pitch, yaw) = euler_from_quaternion (orientation_list)
+    print(yaw)
+
+#   Move robot
     move.linear.x = 0.5
     move.linear.z = 0.0
     pub.publish(move)
