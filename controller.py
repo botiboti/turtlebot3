f1dd45e701cf0daf4a35269bdae0a338f7831a18
Botond Ferenczi <botondferenczi98@gmail.com>
2022-03-30 13:29:49 +0300

    testing dt, publish and subscribe

--- controller.py
+++ controller.py
@@ -1,28 +1,16 @@
 import rospy
-from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 
 def callback(msg):
-    print('s1 [270]')
-    print(msg.ranges[270])
-    print('s2 [0]')
-    print(msg.ranges[0])
-    print('s3 [90]')
-    print(msg.ranges[90])
-
     now = rospy.get_rostime()
     print(rospy.loginfo("Secs: %i Nsecs: %i", now.secs, now.nsecs))
-    if msg.ranges[0] > 0.5:
-        move.linear.x = 0.1 
-        move.linear.z = 0.5
-    else:
-        move.linear.x = 0.0
-        move.linear.z = 0.0
-
+    move.linear.x = 0.5
+    move.linear.z = 0.0
     pub.publish(move)
 
-rospy.init_node('obstacle_avoidance')
-sub = rospy.Subscriber('/scan', LaserScan, callback)
+rospy.init_node('kick_control')
+sub = rospy.Subscriber('/odom', Odometry, callback)
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 move = Twist()
 
