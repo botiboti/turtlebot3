fbf024f04d239df8ac679c27c4ae3e5ee0303d9a
boti <botondferenczi98@gmail.com>
2023-03-27 20:35:47 +0300

    kommenteltem, uj python script, noetic support

--- pythonscripts/controller.py
+++ pythonscripts/controller.py
@@ -7,8 +7,10 @@ from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
 def callback(msg):
 # Print time
+
   now = rospy.get_rostime()
   print(rospy.loginfo("Secs: %i Nsecs: %i", now.secs, now.nsecs))
+  
 # print(msg.pose.pose)
 
 # Get euler from quaternion
@@ -18,20 +20,26 @@ def callback(msg):
 
 # print(yaw)
 
-  act_pos_left =  msg.position.left
+# az aktualis poziciok(rad) es sebessegek(rad/ms ????)
 
+# act_pos_right = msg.position.right
+# act_vel_left = msg.velocity.left
+# act_vel_right = msg.velocity.right
 
 
 
 # a ros_core branch szerinti turtlebot_core-ban linear-nak x meg y
 # valtozojat hasznaljuk a kulonbozo motrok sebbesegenek megadasara
-  move.linear.x = 0.5
-  move.linear.y = 0.0
+  move.linear.x = 100
+  move.linear.y = 100
+  print(move)
   pub.publish(move)
 
 rospy.init_node('controller')
+rospy.loginfo("To stop TurtleBot CTRL + C")
 sub = rospy.Subscriber('/joint_states', JointState, callback)
 pub = rospy.Publisher('/cmd_vels', Twist, queue_size=10)
+# rospy.on_shutdown(pub.publish(Twist())) # ures twist 0.0kal
 move = Twist()
 
 rospy.spin()
---
+++ pythonscripts/testnew.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import JointState
+from geometry_msgs.msg import Twist
+
+class Controller():
+    def __init__(self):
+        # initiliaze
+        rospy.init_node('controller', anonymous=False)
+
+        # tell user how to stop TurtleBot
+        rospy.loginfo("To stop TurtleBot CTRL + C")
+
+        # What function to call when you ctrl + c    
+        rospy.on_shutdown(self.shutdown)
+        
+        # Create a publisher which can "talk" to TurtleBot and tell it to move
+        # Tip: You may need to change cmd_vel_mux/input/navi to /cmd_vel if you're not using TurtleBot2
+        self.cmd_vels = rospy.Publisher('cmd_vels', Twist, queue_size=10)
+        #  joint_states = rospy.Subscriber('/joint_states', JointState, callback)
+     
+        #TurtleBot will stop if we don't keep telling it to move.  How often should we tell it to move? 10 HZ
+        r = rospy.Rate(10);
+
+        # as long as you haven't ctrl + c keeping doing...
+        while not rospy.is_shutdown():
+            now = rospy.get_rostime()
+            rospy.loginfo("Secs: %i Nsecs: %i", now.secs, now.nsecs)
+
+            # ide jon a rugos fuggv., kell def.
+            
+            # Twist is a datatype for velocity, linear.x egyik motor, mas
+            # move_cmd = Twist()
+            # let's go forward at 0.2 m/s
+            # move_cmd.linear.x = 10
+            # let's turn at 0 radians/s
+            # move_cmd.linear.y = 0
+
+            # publish the velocity
+            self.cmd_vels.publish(move_cmd)
+            # wait for 0.1 seconds (10 HZ) and publish again
+            r.sleep()
+
+    def callback(data):
+        now = rospy.get_rostime()
+        rospy.loginfo(data.position)
+
+    def shutdown(self):
+        # stop turtlebot
+        rospy.loginfo("Stop TurtleBot")
+        # a default Twist has linear.x of 0 and angular.z of 0.  So it'll stop TurtleBot
+        self.cmd_vels.publish(Twist())
+        # sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
+        rospy.sleep(1)
+ 
+if __name__ == '__main__':
+    try:
+        Controller()
+    except:
+        rospy.loginfo("Controller node terminated.")
+
