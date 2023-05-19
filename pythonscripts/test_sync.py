01a092051f03c301bd3b9ea13075bc783fea8834
Botond Ferenczi <botondferenczi98@gmail.com>
2023-05-19 13:53:25 +0300

    test for one dim sys

--- pythonscripts/test_sync.py
+++ pythonscripts/test_sync.py
@@ -1,23 +1,45 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 from sensor_msgs.msg import JointState
-rospy.loginfo("Started")
 from message_filters import ApproximateTimeSynchronizer, Subscriber
 
-def synchronized_callback(scan_msg, joint_states_msg):
-    # Process synchronized laser scan and joint states data
-    # Example: Print the ranges from the laser scan and joint positions
-    now = rospy.get_rostime()
-    rospy.loginfo("Secs: %i Nsecs: %i", now.secs, now.nsecs)
-    # print("Laser Ranges:", scan_msg.ranges)
-    # print("Joint Positions:", joint_states_msg.position)
+###############################################################
+## ApproximateTimeSynchronizer kell használni, TimeSynchronizer
+## nem ment                                                 
+###############################################################
+if __name__ == '__main__':
+    try:
+        def synchronized_callback(scan_msg, joint_states_msg):
+            # Process synchronized laser scan and joint states data
+            now = rospy.get_rostime()
+            rospy.loginfo("Secs: %i Nsecs: %i", now.secs, now.nsecs)
+            rospy.loginfo("Secs: %i Nsecs: %i", now.secs, now.nsecs)
+            rospy.loginfo(joint_sub.position[0])
+            rospy.loginfo(laser_sub.angle_increment)
 
-rospy.init_node('synchronized_node', anonymous=True)
-scan_sub = Subscriber('scan', LaserScan)
-joint_states_sub = Subscriber('joint_states', JointState)
-ts = ApproximateTimeSynchronizer([scan_sub, joint_states_sub], 1, 1)
-# You can also set a slop time (in seconds) using: ts.setSlopTime(slop_time)
-ts.registerCallback(synchronized_callback)
-rospy.spin()
+            # x is left wheel
+            # y is right wheel
+            # core part comes here
 
+            move.linear.x = 0.1
+            move.linear.y = 0.1
 
+            twist_pub.publish(move)
+
+        rospy.init_node('Synchronized node', anonymous=True)
+
+        scan_sub = Subscriber('scan', LaserScan)
+        joint_states_sub = Subscriber('joint_states', JointState)
+        twist_pub = rospy.Publisher('/cmd_vels', Twist, queue_size=10)
+        move = Twist()
+        # max linear speed for turtlebot is around 0.2 m/s which is around 6 rad/s
+        omega = 4.
+        ts = ApproximateTimeSynchronizer([scan_sub, joint_states_sub], 1, 1)
+
+        # You can also set a slop time (in seconds) using: ts.setSlopTime(slop_time)
+        ts.registerCallback(synchronized_callback)
+        rospy.spin()
+    except:
+        # send empty Twist with zeros to stop the robot
+        twist_pub.publish(Twist())
+        ("Synchronized node terminated.")
